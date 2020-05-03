dc1454b1497e2c764d9aea18615189f1f49bb27c
fonsecaBarreto <lucasbfonte@gmail.com>
2020-05-03 01:44:13 -0300

    os dois

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const midlewares = require("./src/config/midlewares");
 const routes = require("./src/routes/routes");
 /*  */
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9000;
 const app = express();
 app.use("/files",express.static(__dirname+"/temp/uploads"))
 midlewares(app);
