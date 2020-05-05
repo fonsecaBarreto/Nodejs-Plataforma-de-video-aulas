4b8c542dfa7b6e928eb3a7c861fcba5bbe3e4300
fonsecaBarreto <lucasbfonte@gmail.com>
2020-05-05 13:42:15 -0300

    forcing

--- src/config/midlewares.js
+++ src/config/midlewares.js
@@ -3,7 +3,7 @@ module.exports = app =>{
   app.use(cors())
   app.use(bp.json());
   app.use(bp.urlencoded({extended:false}))
-  if(process.env.NODE_ENV != "dev"){
+  if(process.env.NODE_ENV!="dev"){
     console.log("running https")
     app.use(require('force-https'))
   }
