b9c8f6759659759f5da502b0cab9ca7c862308f6
fonsecaBarreto <lucasbfonte@gmail.com>
2020-05-06 13:12:21 -0300

    danger

--- src/config/midlewares.js
+++ src/config/midlewares.js
@@ -3,8 +3,4 @@ module.exports = app =>{
   app.use(cors())
   app.use(bp.json());
   app.use(bp.urlencoded({extended:false}))
-  if(process.env.NODE_ENV!="dev"){
-    console.log("running https")
-    app.use(require('force-https'))
-  }
 }
